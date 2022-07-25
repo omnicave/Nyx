4793133d74ef9afabfef702612aa354311c1b5bf
George Vella <mail@georgevella.com>
2022-07-25 09:04:59 +0200

    Chore(nyx.cli): cleanup of extension

---
+++ src/CommandLine/Nyx.Cli/Logging/Extensions/CommandLineHostBuilderExtensions.cs
@@ -0,0 +1,16 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nyx.Cli;
+using Nyx.Cli.Logging;
+using Nyx.Cli.Rendering;
+
+// ReSharper disable once CheckNamespace
+namespace Nyx.Cli;
+
+public static partial class CommandLineHostBuilderExtensions
+{
+    public static ICommandLineHostBuilder AddLogLevelOption(this ICommandLineHostBuilder builder)
+    {
+        return builder.AddGlobalOption<LogLevelOption>();
+    }
+}
