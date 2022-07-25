8e2867dc659fae126fef053325a5328bd53fb72c
George Vella <mail@georgevella.com>
2022-07-25 11:05:53 +0200

    fix(ci-cd): add commit count

--- scripts/build-and-pack.sh
+++ scripts/build-and-pack.sh
@@ -6,16 +6,20 @@ echo "${REPO_ROOT}"
 
 VERSION_PREFIX=$(yq -p xml .Project.PropertyGroup.VersionPrefix ${REPO_ROOT}/Directory.Build.props)
 
+COMMIT_COUNT=$(git rev-list --count HEAD)
+
 SHORT_HASH=$(git rev-parse --short HEAD)
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
 NUGET_REPO_NAME="github-georgevella"
 
+VERSION_SUFFIX="${CURRENT_BRANCH}.${SHORT_HASH}"
+
 
 rm -rf ./packages
 
 dotnet clean
-dotnet build -c Release --version-suffix "${CURRENT_BRANCH}.${SHORT_HASH}"
-dotnet pack -c Release --version-suffix "${CURRENT_BRANCH}.${SHORT_HASH}" -o ./packages
+dotnet build -c Release --version-suffix "${VERSION_SUFFIX}"
+dotnet pack -c Release --version-suffix "${VERSION_SUFFIX}" -o ./packages
 
 dotnet nuget push ./packages/*.nupkg --skip-duplicate -s ${NUGET_REPO_NAME}
