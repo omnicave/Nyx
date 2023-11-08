2ac6b47e1067403210d4dd5bceb2a60a0ce1952e
George Vella <george.vella@river.tech>
2023-11-08 20:56:24 +0100

    chore: serialization cleanup

--- src/Orleans/Nyx.Orleans.Client/Serialization/NewtonsoftJsonSerializerSettingsBuilder.cs
+++ src/Orleans/Nyx.Orleans.Client/Serialization/NewtonsoftJsonSerializerSettingsBuilder.cs
@@ -16,11 +16,12 @@ public static class NewtonsoftJsonSerializerSettingsBuilder
         ConfigureJsonSerializerSettingsWithDefaults(jsonSerializerSettings);
         return jsonSerializerSettings;
     }
-    
+
     public static JsonSerializerSettings GetDefaultsWithOrleansSupport()
     {
         var settings = GetDefaults();
         settings.Converters.Add(new EventSequenceTokenConverter());
+        settings.Converters.Add(new NewtonsoftJsonSiloAddressConverter());
 
         return settings;
     }
@@ -28,7 +29,7 @@ public static class NewtonsoftJsonSerializerSettingsBuilder
     public static JsonSerializerSettings GetDefaultsWithOrleansSupport(
         TypeResolver typeResolver,
         GrainReferenceActivator grainReferenceActivator
-        )
+    )
     {
         var settings = GetDefaultsWithOrleansSupport();
         var serializationBinder = new OrleansJsonSerializationBinder(typeResolver);
@@ -53,8 +54,6 @@ public static class NewtonsoftJsonSerializerSettingsBuilder
         jsonSerializerSettings.Converters.Add(new IPAddressConverter());
         jsonSerializerSettings.Converters.Add(new IPEndPointConverter());
         jsonSerializerSettings.Converters.Add(new GrainIdConverter());
-        jsonSerializerSettings.Converters.Add(new NewtonsoftJsonSiloAddressConverter());
         jsonSerializerSettings.Converters.Add(new UniqueKeyConverter());
-
     }
 }
