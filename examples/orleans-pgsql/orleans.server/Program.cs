29da96e0f88e8eb324d81b7ef2da82ae5dc9f43e
georgevella <georgevella@gmail.com>
2026-03-05 20:25:50 +0100

    chore(examples): update Orleans pgsql sample

--- examples/orleans-pgsql/orleans.server/Program.cs
+++ examples/orleans-pgsql/orleans.server/Program.cs
@@ -5,25 +5,28 @@ using Nyx.Orleans.Host;
 using Nyx.Orleans.Nats;
 using Orleans;
 
-var builder = OrleansSiloHostBuilder.CreateSiloHost("ExampleOrleansCluster", "Ex1", args: args);
+var hostBuilder = WebApplication.CreateBuilder(args)
+    .ConfigureSiloHost("ExampleOrleansCluster", "Ex1");
 
-builder
-    .ConfigureClustering(sb => sb.AddNatsClustering())
-    .ConfigureOrleansSilo(
-        (context, siloBuilder) =>
-        {
-            siloBuilder
-                .AddNatsStreams(orleans.shared.Constants.NatsStreamProviderName);
-        }
-        )
-    // .ConfigureForPostgresClustering(
-    //     "Host=localhost; Port=54321; Database=postgres;Username=postgres;Password=password123")
-    .UsePostgresPubSubStore(
-        "Host=localhost; Port=54321; Database=postgres;Username=postgres;Password=password123")
-    .UsePostgresInternalGrainStorage(
-        "Host=localhost; Port=54321; Database=postgres;Username=postgres;Password=password123")
-    ;
+// var builder = OrleansSiloHostBuilder.CreateSiloHost("ExampleOrleansCluster", "Ex1", args: args);
+//
+// builder
+//     .ConfigureClustering(sb => sb.AddNatsClustering())
+//     .ConfigureOrleansSilo(
+//         (context, siloBuilder) =>
+//         {
+//             siloBuilder
+//                 .AddNatsStreams(orleans.shared.Constants.NatsStreamProviderName);
+//         }
+//         )
+//     // .ConfigureForPostgresClustering(
+//     //     "Host=localhost; Port=54321; Database=postgres;Username=postgres;Password=password123")
+//     .UsePostgresPubSubStore(
+//         "Host=localhost; Port=54321; Database=postgres;Username=postgres;Password=password123")
+//     .UsePostgresInternalGrainStorage(
+//         "Host=localhost; Port=54321; Database=postgres;Username=postgres;Password=password123")
+//     ;
 
-var host = builder.Build();
+var host = hostBuilder.Build();
 
 await host.RunAsync();
